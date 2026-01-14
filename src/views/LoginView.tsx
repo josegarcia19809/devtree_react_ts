7a18dbdfd32f229552db8f4b10e2fe257c435487
Jose Garcia <josegarcia19809@gmail.com>
2026-01-13 19:18:59 -0600

    DevTree React - Navegando entre páginas con React Router

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,6 +1,17 @@
+import {Link} from "react-router-dom";
+
 function LoginView() {
     return (
-        <></>
+        <>
+            <div>
+                Ingresa
+            </div>
+            <nav>
+                <Link to="/auth/register">
+                    ¿No tienes cuenta? Crea una aquí
+                </Link>
+            </nav>
+        </>
     );
 }
 
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,6 +1,17 @@
+import {Link} from "react-router-dom";
+
 function RegisterView() {
     return (
-        <></>
+        <>
+            <div>
+                Registrate
+            </div>
+            <nav>
+                <Link to="/auth/login">
+                    ¿Ya tienes cuenta? Inicia
+                </Link>
+            </nav>
+        </>
     );
 }
 
