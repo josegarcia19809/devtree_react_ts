cc2c2a99777d3cf50e959a4154446336a7e26b43
Jose Garcia <josegarcia19809@gmail.com>
2026-01-13 19:46:17 -0600

    DevTree React - Creando el Layout Principal para Autenticacion

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,13 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginView from "./views/LoginView.tsx";
 import RegisterView from "./views/RegisterView.tsx";
+import AuthLayout from "./layouts/AuthLayout.tsx";
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route>
+                <Route element={<AuthLayout/>}>
                     <Route path='/auth/login' element={<LoginView/>}/>
                     <Route path='/auth/register' element={<RegisterView/>}/>
                 </Route>
