42cd034c0cffba5be70704933d7561201396e7a6
Jose Garcia <josegarcia19809@gmail.com>
2026-02-05 07:30:33 -0600

    DevTree React - Proteger el Panel de Administración si un usuario no esta autenticado

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,14 +3,20 @@ import {Toaster} from "sonner";
 import NavigationTabs from "../components/NavigationTabs";
 import {useQuery} from "@tanstack/react-query";
 import {getUser} from "../api/DevTreeAPI.ts";
+import {Navigate} from "react-router-dom";
 
 export default function AppLayout() {
-    const {data, isLoading, error, isError} = useQuery({
+    const {data, isLoading, isError} = useQuery({
         queryFn: getUser,
         queryKey: ['user'],
         retry:1,
         refetchOnWindowFocus: false,
     })
+
+    if (isLoading) {return "Loading..."}
+    if (isError) {
+        return <Navigate to="/auth/login"></Navigate>
+    }
 
     return (
         <>
