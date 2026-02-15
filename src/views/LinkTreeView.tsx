8bfc86d569080a6a4b8b02473a139787277c3659
Jose Garcia <josegarcia19809@gmail.com>
2026-02-14 18:33:54 -0600

    DevTree React - Añadiendo links a nuestro modelo de usuarios

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -4,10 +4,23 @@ import DevTreeInput from "../components/DevTreeInput.tsx";
 import * as React from "react";
 import {isValidUrl} from "../utils";
 import {toast} from "sonner";
+import {useMutation} from "@tanstack/react-query";
+import {updateProfile} from "../api/DevTreeAPI.ts";
 
 
 const LinkTreeView = () => {
     const [devTreeLinks, setDevTreeLinks] = useState(social)
+
+    const {mutate} = useMutation({
+        mutationFn: updateProfile,
+        onError: (err) => {
+            toast.error(err.message)
+        },
+        onSuccess: () => {
+            toast.success("Actualizado com sucesso!")
+        }
+    })
+
 
     const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const updatedLinks = devTreeLinks.map((link) => link.name === e.target.name ? {
@@ -43,6 +56,13 @@ const LinkTreeView = () => {
                         handleEnableLink={handleEnableLink}
                     />
                 ))}
+                <button
+                    className="bg-blue-600 hover:bg-blue-700 text-white font-semibold
+         py-2 px-6 rounded-xl shadow-md w-full
+         transition duration-300 ease-in-out
+         transform hover:scale-105 active:scale-95">
+                    Guardar
+                </button>
             </div>
         </>
     )
