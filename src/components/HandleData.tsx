b360418d7cfa1f4085a24fa8977b0fe4fac084ab
Jose Garcia <josegarcia19809@gmail.com>
2026-02-18 13:16:19 -0600

    DevTree React - Mostrando Handle, Imagen y Descripción

--- src/components/HandleData.tsx
+++ src/components/HandleData.tsx
@@ -8,7 +8,10 @@ function HandleData({data}: HandleDataProps) {
     return (
         <>
             <div className="space-y-6 text-white">
-                <p className="text-5xl text-center "> {data.handle}</p>
+                <p className="text-5xl text-center font-black"> {data.handle}</p>
+                {data.image &&
+                    <img className="max-w-[250px] mx-auto" src={data.image} alt=""/>}
+                <p className="lg text-center font-bold"> {data.description}</p>
             </div>
         </>
     );
