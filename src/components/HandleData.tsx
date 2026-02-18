f5af8db9551b3bf6b3dd5903bb8bf309e163ffc9
Jose Garcia <josegarcia19809@gmail.com>
2026-02-18 13:41:57 -0600

    DevTree React - Mostrando los enlaces activos del Usuario

--- src/components/HandleData.tsx
+++ src/components/HandleData.tsx
@@ -1,10 +1,13 @@
-import type {UserHandle} from "../types";
+import type {SocialNetwork, UserHandle} from "../types";
 
 type HandleDataProps = {
     data: UserHandle
 }
 
 function HandleData({data}: HandleDataProps) {
+    const links: SocialNetwork[] = JSON.parse(data.links)
+        .filter((link: SocialNetwork) => link.enabled)
+
     return (
         <>
             <div className="space-y-6 text-white">
@@ -12,6 +15,26 @@ function HandleData({data}: HandleDataProps) {
                 {data.image &&
                     <img className="max-w-[250px] mx-auto" src={data.image} alt=""/>}
                 <p className="lg text-center font-bold"> {data.description}</p>
+                <div className="mt-20 flex flex-col  gap-6">
+                    {links.length ? (
+                        links.map((link: SocialNetwork) => (
+                            <a
+                                className="bg-white px-5 py-2 flex items-center gap-5
+                                rounded-xl"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href={link.url} key={link.id}>
+                                <img src={`/social/icon_${link.name}.svg`} alt="red social"
+                                className="w-12"/>
+                                <p className="text-black font-black text-xl">Visítame en
+                                    <span className="capitalize"> {link.name}</span></p>
+                            </a>
+                        ))
+                    ) : (
+                        <p className="text-center text-white">No hay enlaces en este
+                            perfil</p>
+                    )}
+                </div>
             </div>
         </>
     );
